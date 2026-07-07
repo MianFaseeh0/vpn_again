3cd8a6edc89f9d2e8f8315a618fd52771baa407a
Mian Faseeh <mianfaseeh@Mians-MacBook-Air.local>
2026-07-07 11:31:48 +0500

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import 'package:vpnapp/core/theme/app_theme.dart';
 import 'package:vpnapp/features/auth/presentation/providers/auth_providers.dart';
 import 'package:vpnapp/features/auth/presentation/views/welcome_view.dart';
